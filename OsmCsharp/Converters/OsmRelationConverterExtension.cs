3945fda3be995f7dbba6bbaad8970b50b4ea3930
Arlindo "Nighto" Pereira <nighto@nighto.net>
2011-05-29 17:00:51 -0300

    Creating empty relation converter extensions.

--- OsmCsharp/Converters/OsmRelationConverterExtension.cs
+++ OsmCsharp/Converters/OsmRelationConverterExtension.cs
@@ -7,34 +7,34 @@ using System.Xml;
 
 namespace OsmCsharp.Converters
 {
-    public static class OsmWayConverterExtension
+    public static class OsmRelationConverterExtension
     {
-        public static string ToString(this OsmWay way)
+        public static string ToString(this OsmRelation relation)
         {
             throw new NotImplementedException();
         }
 
-        public static XmlNode ToXml(this OsmWay way)
+        public static XmlNode ToXml(this OsmRelation relation)
         {
             throw new NotImplementedException();
         }
 
-        public static XmlDocument ToFullXml(this OsmWay way)
+        public static XmlDocument ToFullXml(this OsmRelation relation)
         {
             throw new NotImplementedException();
         }
 
-        public static string ToXmlString(this OsmWay way)
+        public static string ToXmlString(this OsmRelation relation)
         {
             throw new NotImplementedException();
         }
 
-        public static string ToFullXmlString(this OsmWay way)
+        public static string ToFullXmlString(this OsmRelation relation)
         {
             throw new NotImplementedException();
         }
 
-        public static string ToJson(this OsmWay way)
+        public static string ToJson(this OsmRelation relation)
         {
             throw new NotImplementedException();
         }
--- OsmCsharp/Elements/OsmRelation.cs
+++ OsmCsharp/Elements/OsmRelation.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using OsmCsharp.Converters;
+using System.Xml;
 
 namespace OsmCsharp.Elements
 {
@@ -9,5 +11,35 @@ namespace OsmCsharp.Elements
     {
         public List<RelationMember> MembersList { get; set; }
         public List<Tag> TagsList { get; set; }
+
+        public override string ToString()
+        {
+            return OsmRelationConverterExtension.ToString(this);
+        }
+
+        public XmlNode ToXml()
+        {
+            return OsmRelationConverterExtension.ToXml(this);
+        }
+
+        public XmlDocument ToFullXml()
+        {
+            return OsmRelationConverterExtension.ToFullXml(this);
+        }
+
+        public string ToXmlString()
+        {
+            return OsmRelationConverterExtension.ToXmlString(this);
+        }
+
+        public string ToFullXmlString()
+        {
+            return OsmRelationConverterExtension.ToFullXmlString(this);
+        }
+
+        public string ToJson()
+        {
+            return OsmRelationConverterExtension.ToJson(this);
+        }
     }
 }
